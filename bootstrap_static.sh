6ebc243af72b6d943eabf42f7022e52bb8926184
Fabien Andrieux <fandrieu@cisco.com>
2017-03-14 01:42:12 +0100

    Add ANSIBLE_LIBRARY PATH for sitefacts.py

--- bootstrap_static.sh
+++ bootstrap_static.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 VARS="${VARS} ANSIBLE_SCP_IF_SSH=y ANSIBLE_HOST_KEY_CHECKING=False"
-
+export ANSIBLE_LIBRARY=$PWD/playbooks/library
 export $VARS
 ansible-playbook -f 20 -i inventory/static playbooks/bootstrap.yml
