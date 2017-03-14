1d4fd98bba07c77424cb6a90f5b0ebaeac2abf5e
Fabien Andrieux <fandrieu@cisco.com>
2017-03-14 01:10:15 +0100

    Force initializing ANSIBLE_LIBRARY to retrieve playbooks/library/sitefacts.py

--- hortonworks_static.sh
+++ hortonworks_static.sh
@@ -3,4 +3,5 @@
 VARS="${VARS} ANSIBLE_SCP_IF_SSH=y ANSIBLE_HOST_KEY_CHECKING=False"
 
 export $VARS
+export ANSIBLE_LIBRARY=$PWD/playbooks/library
 ansible-playbook -f 20 -i inventory/static playbooks/hortonworks.yml
